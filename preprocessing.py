b2d28898e2bde9c386098aa03326d6dbda18aff1
soumi7 <soumibardhan10@gmail.com>
2020-11-22 22:14:12 +0530

    Added preprocessing file and csv.

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,14 @@
 import os
 import PyPDF2 as pdf
+import pandas as pd
 
 
 csv_data=[]
-directory="/home/soumi/data_to_preprocess"
+directory="/home/soumi/EY_GDS_Project/data_to_preprocess/"
 for filename in os.listdir(directory):
     if filename.endswith(".pdf"): 
         file = open(os.path.join(directory, filename),'rb')
-        pdf_reader = pdf.PdfFileReader(file)
+        read_pdf = pdf.PdfFileReader(file)
         number_of_pages = read_pdf.getNumPages()
         pdf_content=""
         for page_number in range(number_of_pages):   # use xrange in Py2
@@ -15,7 +16,7 @@ for filename in os.listdir(directory):
             page_content = page.extractText()
             pdf_content+=page_content
            
-        csv_data.append({"PDF_Name":filename,'text':pdf_content,'intent':""})
+    csv_data.append({"PDF_Name":filename,'text':pdf_content,'intent':""})
         
-df = pd.DataFrame(csv_data, index=False)
+df = pd.DataFrame(csv_data)
 df.to_csv('text_intent.csv') 
