612314e91eb5ea9b1fea957e162d59b328e6942b
Soumi Bardhan <51290447+Soumi7@users.noreply.github.com>
2020-11-22 21:31:25 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -11,13 +11,15 @@ directory="/home/soumi/dataset_intent"
 for filename in os.listdir(directory):
     if filename.endswith(".jpg"): 
         file = open(os.path.join(directory, filename),'rb')
-        
+        pdf_reader = pdf.PdfFileReader(file)
+        number_of_pages = read_pdf.getNumPages()
+        for page_number in range(number_of_pages):   # use xrange in Py2
+            page = read_pdf.getPage(page_number)
+            page_content = page.extractText()
+            text_file.write(page_content)
+           
         csv_data.append({"IMG_NAME":filename,'area':area1,'count_of_cancerous_cells':counter1,'cancer_type':os.path.basename(os.path.normpath(directory))})
-        if os.path.basename(os.path.normpath(directory))=="cancerous":
-            k=1
-        else:
-            k=0
-        csv_data.append({'area':area1,'count_of_cancerous_cells':counter1,'cancer_type':k})
+        
 
 df = pd.DataFrame(csv_data, index=False)
-df.to_csv('non_cancerous.csv') 
+df.to_csv('text_intent.csv') 
