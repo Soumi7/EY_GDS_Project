86a3eebe1ad4c334e4f5c253df9851d64317578d
soumi7 <soumibardhan10@gmail.com>
2020-11-26 11:44:22 +0530

    u

--- API_access_code_snippet.py
+++ API_access_code_snippet.py
@@ -2,3 +2,5 @@ import requests
 from requests_ntlm import HttpNtlmAuth
 
 requests.get("http://sharepoint-site.com", auth=HttpNtlmAuth('DOMAIN\\USERNAME','PASSWORD'))
+
+#https://www.pharmasug.org/proceedings/2020/AD/PharmaSUG-2020-AD-341.pdf
