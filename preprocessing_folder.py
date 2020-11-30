942e7cc1b25ce618e5db67b8d917fa8e6ba39c7d
soumi7 <soumibardhan10@gmail.com>
2020-11-30 17:59:10 +0530

    u

--- preprocessing_folder.py
+++ preprocessing_folder.py
@@ -23,26 +23,29 @@ intent=[]
 directory="/home/soumi/EY_GDS_Project/EY_DATA/"
 
 for filename in os.listdir(directory):
-    output_string = StringIO()
-    with open(os.path.join(directory, filename), 'rb') as in_file:
-        parser = PDFParser(in_file)
-        doc = PDFDocument(parser)
-        rsrcmgr = PDFResourceManager()
-        device = TextConverter(rsrcmgr, output_string, laparams = LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        for page in PDFPage.create_pages(doc):
-            interpreter.process_page(page)
-    print(type(output_string.getvalue()))
-    print(type(output_string))
-    text=output_string.getvalue()
-    data = sent_tokenize(text)
-    for line in data:
-        res=re.sub('\s+',' ',line)
-        line=str(res)
-        file_names.append(filename)
-        sentence.append(line)
-        label.append(0)
-        intent.append(filename.split(".")[0])
+    if filename.endswith('.pdf'):
+        output_string = StringIO()
+        with open(os.path.join(directory, filename), 'rb') as in_file:
+            parser = PDFParser(in_file)
+            doc = PDFDocument(parser)
+            rsrcmgr = PDFResourceManager()
+            device = TextConverter(rsrcmgr, output_string, laparams = LAParams())
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            for page in PDFPage.create_pages(doc):
+                interpreter.process_page(page)
+        print(type(output_string.getvalue()))
+        print(type(output_string))
+        text=output_string.getvalue()
+        data = sent_tokenize(text)
+        for line in data:
+            res=re.sub('\s+',' ',line)
+            line=str(res)
+            file_names.append(filename)
+            sentence.append(line)
+            label.append(0)
+            intent.append(filename.split(".")[0])
+    if file.endswith(".csv"):
+        
         
 #df = pd.DataFrame(csv_data)
 df = pd.DataFrame(list(zip(file_names, sentence , label, intent)) , columns=["Filename", "Sentence" , "Label", "Intent"])
