97549d35bb92fae7d4badb5487bbe8e68e2f4fdf
loh04 <lohithmunakala@gmail.com>
2020-12-05 03:03:17 +0530

    .

--- data_split.py
+++ data_split.py
@@ -1,28 +1,28 @@
-# import sklearn
-# import pandas as pd
-# import numpy as np
-# from sklearn.utils import shuffle
+import sklearn
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
 
-# df = pd.read_csv("folders.csv", encoding="utf-8-sig")
+df = pd.read_csv("folders.csv", encoding="utf-8-sig")
 
-# df =  df[["Sentence","Intent"]]
+df =  df[["Sentence","Intent"]]
 
-# df = shuffle(df)
+df = shuffle(df)
 
-# size =  len(df)
+size =  len(df)
 
-# train_df = df[:int(np.round(size*.6, 0))]
-# test_df = df[int(np.round(size*.6, 0)) : int(np.round(size*.6, 0))+int(np.round(size*.2, 0))]
-# valid_df = df[int(np.round(size*.6, 0))+int(np.round(size*.2, 0)):]
+train_df = df[:int(np.round(size*.6, 0))]
+test_df = df[int(np.round(size*.6, 0)) : int(np.round(size*.6, 0))+int(np.round(size*.2, 0))]
+valid_df = df[int(np.round(size*.6, 0))+int(np.round(size*.2, 0)):]
 
 
-# train_df.to_csv('train.csv',encoding='utf-8-sig', index=False) 
-# test_df.to_csv('test.csv',encoding='utf-8-sig', index=False) 
-# valid_df.to_csv('valid.csv',encoding='utf-8-sig', index=False) 
+train_df.to_csv('train.csv',encoding='utf-8-sig', index=False) 
+test_df.to_csv('test.csv',encoding='utf-8-sig', index=False) 
+valid_df.to_csv('valid.csv',encoding='utf-8-sig', index=False) 
 
-import re
+# import re
 
-s = "2 3 4 5 lohith 34"
+# s = "2 3 4 5 lohith 34"
 
-s = re.sub(r"\d+\W+|\b\d+\b|\W+\d+", "", s)
-print(s)
+# s = re.sub(r"\d+\W+|\b\d+\b|\W+\d+", "", s)
+# print(s)
