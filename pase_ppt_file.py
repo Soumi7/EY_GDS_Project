4aec28e21c69f8cd7f6bd506f145789189f4ca4b
soumi7 <soumibardhan10@gmail.com>
2020-12-05 14:26:54 +0530

    ppt parser

--- pase_ppt_file.py
+++ pase_ppt_file.py
@@ -5,7 +5,18 @@ from pptx import Presentation
 
 prs = Presentation("/home/soumi/Downloads/EYintentRepository/Repository 1/Item 4/azure-250215.pptx")
 print("----------------------")
+'''
 for slide in prs.slides:
     for shape in slide.shapes:
         if hasattr(shape, "text"):
             print(shape.text)
+'''
+content = ""
+slideCount = 0
+for slide in prs.slides:
+    slideCount += 1
+    for shape in slide.shapes:
+        if (shape.has_text_frame):
+            for paragraph in shape.text_frame.paragraphs:
+                for run in paragraph.runs:
+                    print(run.text)
