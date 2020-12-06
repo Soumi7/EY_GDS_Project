b25a1d67499cd673306bfc5e7ff9a80e0019fbaa
loh04 <lohithmunakala@gmail.com>
2020-12-06 17:29:55 +0530

    adding the vocab and test file

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import pandas as pd 
 import numpy as np
 
-df  = pd.read_csv("folders.csv", encoding ="utf-8-sig")
+df  = pd.read_csv("train.csv", encoding ="utf-8-sig")
+df1 = pd.read_csv("folders.csv", encoding ="utf-8-sig")
 
-print(df["Intent"].value_counts())
+
+print(df["intent"].value_counts())
+print()
+print(df1["Intent"].value_counts())
 
 # print(len(df["File Name"].unique()))
