4847b5356e555f665cf6356e481028569495706a
soumi7 <soumibardhan10@gmail.com>
2020-12-07 21:59:22 +0530

    removing unwanted files.

--- API_access_code_snippet.py
+++ API_access_code_snippet.py
@@ -3,4 +3,4 @@ from requests_ntlm import HttpNtlmAuth
 
 requests.get("http://sharepoint-site.com", auth=HttpNtlmAuth('DOMAIN\\USERNAME','PASSWORD'))
 
-#https://www.pharmasug.org/proceedings/2020/AD/PharmaSUG-2020-AD-341.pdf
+#for sharepoint API access in future
